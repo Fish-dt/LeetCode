e220ba9debdf081a07bf82ddf0aa1b0525928711
Fisiha <Fissehadesta70@gmail.com>
2025-10-21 00:24:54 +0300

    Time: 47 ms (85.56%), Space: 20.5 MB (36.46%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        curr=root
+        while curr:
+            if curr.val<p.val and curr.val<q.val:
+                curr=curr.right
+            elif curr.val>p.val and curr.val>q.val:
+                curr=curr.left
+            else:
+                return curr
