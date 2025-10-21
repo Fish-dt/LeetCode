6cf76a4588698fadc2abbe79fcac00187713ba20
Fisiha <Fissehadesta70@gmail.com>
2025-10-21 12:43:16 +0300

    Time: 4 ms (22.08%), Space: 13.1 MB (99.13%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def levelOrder(self, root):
+        res=[]
+        q = collections.deque()
+        q.append(root)
+        while q:
+            level=[]
+            qLen=len(q)
+            for i in range(qLen):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
